e2935646be93eb5342f7135ebe05273296200e26
pulku <pinarul@hotmail.com>
2016-01-31 01:47:15 +0200

    initial commit

---
+++ app/src/main/java/pulku/interactivestory/model/Choice.java
@@ -0,0 +1,30 @@
+package pulku.interactivestory.model;
+
+/**
+ * Created by pınar on 28.01.2016.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
